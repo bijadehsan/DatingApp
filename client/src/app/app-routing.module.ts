df51aaba52bf33424dbdfe8d5c8ef58430fe17a5
ehsan bijad <ehsan.bijad90@gmail.com>
2022-07-25 12:49:08 -0400

    End of section 7

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,7 +5,10 @@ import { MemberListComponent } from '././members/member-list/member-list.compone
 import { MemberDetailComponent } from '././members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
+import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { AuthGuard } from './_guards/auth.guard';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
 
 const routes: Routes = [
   {path:'',component: HomeComponent},
@@ -14,13 +17,17 @@ const routes: Routes = [
     runGuardsAndResolvers:'always',
     canActivate:[AuthGuard],
     children:[
-      {path:'members',component: MemberListComponent,canActivate:[AuthGuard]},
+      {path:'members',component: MemberListComponent},
       {path:'members/:id',component: MemberDetailComponent},
       {path:'lists',component: ListsComponent},
       {path:'messages',component: MessagesComponent},
-      {path:'**',component: HomeComponent,pathMatch:'full'}
+      
     ]
-  }
+  },
+  {path:'not-found', component:NotFoundComponent},
+  {path:'server-error', component:ServerErrorComponent},
+  {path:'errors', component:TestErrorsComponent},
+  {path:'**',component: NotFoundComponent,pathMatch:'full'}
   
 ];
 
